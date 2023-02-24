f83c8266390f938793a80aa38f0a2fb59945333a
Stefanus Adri Irawan <117265543+stefanusadriirawan@users.noreply.github.com>
2023-02-24 09:05:51 +0700

    Update mainGoks.py

--- mainGoks.py
+++ mainGoks.py
@@ -1,7 +1,37 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
+github = '''
+           <style>
+                .button-wrapper {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 10px;
+                    background-color: #FFFFFF;
+                    box-shadow: 0px 0px 10px #888888;
+                    cursor: pointer;
+                    transition: background-color 0.5s ease;
+                }
+                .button-wrapper:hover {
+                    background-color: #87CEFA;
+                }
+                .button-text {
+                    margin-left: 3px;
+                    font-size: 20px;
+                    font-weight: bold;
+                    color: #000000;
+                }
+            </style>
+            <a href="https://github.com/stefanusadriirawan" style="text-decoration:none;">
+                <div class="button-wrapper">
+                    <span class="button-text">Github</span>
+                </div>
+            </a>
+        '''
 # options=["Home", "About", "Skills", "Projects", "Contact"],
 # icons=["house", "person-circle", "code-slash", "person-workspace", "telephone"],
 
@@ -61,6 +91,21 @@ def contact():
     - Phone: +1-123-456-7890
     - LinkedIn: 
     """)
+    col1, col2, col3, col4, col5= st.columns(5)
+    with col1:
+        st.markdown(github, unsafe_allow_html=True)
+    with col2:
+        st.markdown(github, unsafe_allow_html=True)
+    with col3:
+        st.markdown(github, unsafe_allow_html=True)
+    with col4:
+        st.markdown(github, unsafe_allow_html=True)
+    with col5:
+        st.markdown(github, unsafe_allow_html=True)
+
+
+
+
 
 
 # lem pemersatu -----------------------------------------------------------------------------
